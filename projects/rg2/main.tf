b06694106e811a45f08b42b9d9fcb3b553baad74
Sindre Nedrebø <eyecon.sindre@gmail.com>
2025-11-08 01:13:21 +0100

    endret til å bruke env backend

---
+++ projects/rg2/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+backend "azurerm" {
+    # see backend.hcl for non-secret settings
+    # use_azuread_auth = true  # uncomment to use Azure AD auth (instead of access key)
+   }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.40.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+#create resource group
+resource "azurerm_resource_group" "main" {
+  name     = var.rg_name
+  location = var.location
+}
