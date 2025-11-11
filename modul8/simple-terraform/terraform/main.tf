8c565612f3e7d1bb22b5a2759043ab3b5cf4a9c7
Sindre Nedrebø <eyecon.sindre@gmail.com>
2025-11-11 09:36:42 +0100

    endret til GRS

--- modul8/simple-terraform/terraform/main.tf
+++ modul8/simple-terraform/terraform/main.tf
@@ -23,8 +23,8 @@ resource "azurerm_storage_account" "main" {
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
 
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_tier             = "Premium"
+  account_replication_type = "GRS"
 
   min_tls_version = "TLS1_2"
 
